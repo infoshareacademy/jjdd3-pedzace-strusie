c582cfd20b156f311318800cc86579c3ba3b1e12
Konrad Doroszuk <dorosz63@gmx.us>
2018-01-31 16:57:06 +0100

    Stop-work commit

---
+++ src/main/java/com/infoshare/pedzacestrusie/smm/Categories.java
@@ -0,0 +1,16 @@
+package com.infoshare.pedzacestrusie.smm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Categories {
+
+    public static void newList() {
+        List<String> categories = new ArrayList<String>();
+
+        System.out.println("Created new list.");
+    }
+
+
+
+}
--- src/main/java/com/infoshare/pedzacestrusie/smm/Main.java
+++ src/main/java/com/infoshare/pedzacestrusie/smm/Main.java
@@ -3,5 +3,9 @@ package com.infoshare.pedzacestrusie.smm;
 public class Main {
 
     public static void main(String[] args){
+
+        Categories.newList();
+        Categories.categories1.add("Kategoria");
+        System.out.println(Categories.categories1.get(0));
     }
 }
