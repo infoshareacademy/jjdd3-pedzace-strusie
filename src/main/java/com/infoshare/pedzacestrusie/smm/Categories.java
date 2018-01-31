fcda68b54e48f6ed9e4f3d70078e7d58a0bfdde9
Konrad Doroszuk <dorosz63@gmx.us>
2018-01-31 23:50:07 +0100

    Stop-work commit

--- src/main/java/com/infoshare/pedzacestrusie/smm/Categories.java
+++ src/main/java/com/infoshare/pedzacestrusie/smm/Categories.java
@@ -2,15 +2,23 @@ package com.infoshare.pedzacestrusie.smm;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Categories {
 
-    public static void newList() {
-        List<String> categories = new ArrayList<String>();
+    static List<String> list = new ArrayList<String>();
 
-        System.out.println("Created new list.");
+    public static void restoreDefault() {
+
+        System.out.println("Are you sure? Categories will be restored to default.\nType \"YES\" to delete, anything else will abort restoring.");
+        Scanner scanner = new Scanner(System.in);
+        String answer = answer = scanner.next();
+        if(answer.equals("YES")) {
+            list.clear();
+            System.out.println("Deleted.");
+        }
+        else {
+            System.out.println("Aborted.");
+        }
     }
-
-
-
 }
--- src/main/java/com/infoshare/pedzacestrusie/smm/Main.java
+++ src/main/java/com/infoshare/pedzacestrusie/smm/Main.java
@@ -2,10 +2,10 @@ package com.infoshare.pedzacestrusie.smm;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        Categories.newList();
-        Categories.categories1.add("Kategoria");
-        System.out.println(Categories.categories1.get(0));
+        Categories.restoreDefault();
+
+
     }
 }
