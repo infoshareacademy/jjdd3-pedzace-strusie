4d9f93bdfac8276276c48cb70bd201027cce1b40
danieljkaras <35041653+danieljkaras@users.noreply.github.com>
2018-02-08 16:53:25 +0100

    JJDDPED-5 (#2)
    * Initial commit
    
    * Commit include designed classes income and expenses. Also include class File Creator and Write to File.
    
    * Initial commit
    
    * Added menu
    
    * new feature read from file
    
    * Updated chooseMenuItems and added displayMenu
    
    * Added Expenses and Income class
    
    * Update menu and added get to expenses
    
    * some changes into CRUD features
    
    * Added CategoryMenu, ExpensesMenu and IncomeMenu
    
    * features CreateFile and ReadFromFile is ready, feature WriteToFile is in phase production in progress
    
    * commit include progress in feature write to csv file also has printing of data from LinkedList
    
    * in work commit include operartion on WriteCSV
    
    * comiit contain fully working feature: writeCSV, readCSV and createCSV
    
    * commit contain feature createCSV, readCSV and writeCSV
    
    * removed.idea

---
+++ src/main/java/com/infoshare/pedzacestrusie/smm/CategoryMenu.java
@@ -0,0 +1,64 @@
+package com.infoshare.pedzacestrusie.smm;
+
+import java.util.Scanner;
+
+/**
+ * Author: Maciej Wanagos
+ */
+public class CategoryMenu extends Menu{
+
+    //main menu description
+    private String[] categoryMenuItems = {"addCategory();", "showCategories();", "removeCategory();", "restoreDefault();","Exit"};
+
+    public CategoryMenu() {
+        chooseMenuItems(setMenuItems(categoryMenuItems));
+    }
+
+    @Override
+    protected void chooseMenuItems(String menuDescr) {
+
+        int idx = 1;
+
+        //displays menu items
+        System.out.println(menuDescr);
+
+        //selects items from menu
+        while (idx != 0) {
+            try {
+
+                idx = new Scanner(System.in).nextInt();
+
+                if (idx < 1 || idx > categoryMenuItems.length - 1) {
+                    continue;
+                }
+
+                switch (idx) {
+                    case 1: {
+                        //addCategory
+                        System.out.println("Your choice is: " + idx);
+                        break;
+                    }
+                    case 2: {
+                        //showCategories
+                        System.out.println("Your choice is: " + idx);
+                        break;
+                    }
+                    case 3:
+                        //removeCategory
+                        System.out.println("Your choice is: " + idx);
+                        break;
+                    case 4:
+                        //restoreDefault
+                        System.out.println("Your choice is: " + idx);
+                        break;
+                }
+
+            } catch (Exception e) {
+                continue;
+            }
+        }
+
+
+    }
+
+}
