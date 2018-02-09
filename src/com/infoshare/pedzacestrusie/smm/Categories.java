bde7546cd5eab337dbad270a03e97fcaa253b0b4
Konrad Doroszuk <dorosz63@gmx.us>
2018-02-09 16:53:45 +0100

    In-work commit

--- src/com/infoshare/pedzacestrusie/smm/Categories.java
+++ src/com/infoshare/pedzacestrusie/smm/Categories.java
@@ -8,27 +8,30 @@ public class Categories {
     private TreeSet<String> categories = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
 
 
+    Categories() {
+        readCSV readFromFile = new readCSV();
+        categories.addAll(readFromFile("CurrentCategories.txt"));
+    }
+
     public TreeSet<String> getCategories() {
         return categories;
     }
 
-        //user case
-    public void addCategory() {
+    //user case
+    private void addCategory() {
 
         System.out.print("Type new category: \n(\"q\" will abort)\n");
-        Scanner scanner = new Scanner(System.in);
-        String tempScanner = scanner.nextLine();
+        String input = readInput();
 
-        if(tempScanner.equals("q")) {
-            System.out.println("Aborted.");
+        if(isAborted(input)) {
+            return;
         }
-        else {
-            categories.add(tempScanner);
-            System.out.println("Added.");
-        }
+
+        categories.add(input);
+        System.out.println("Added.");
     }
 
-        //developer case
+    //developer case
     public void addCategory(String category) {
         categories.add(category);
     }
@@ -38,53 +41,59 @@ public class Categories {
         System.out.println("Categories: " + categories);
     }
 
-        //user case
+    //user case
     public void removeCategory() {
+        showCategories();
         System.out.print("Type category to remove:\n(\"q\" will abort)\n");
-        Scanner scanner = new Scanner(System.in);
-        String tempScanner = scanner.nextLine();
+        String input = readInput();
 
-        if(tempScanner.equals("q")) {
-            System.out.println("Aborted.");
+        if(isAborted(input)) {
+            return;
         }
-        else if (categories.contains(tempScanner) == false) {
+
+        if (categories.contains(input) == false) {
             System.out.println("There is no such category.");
         }
         else {
-            categories.remove(tempScanner);
+            categories.remove(input);
             System.out.println("Removed.");
         }
     }
 
-        //developer case
+    //developer case
     public void removeCategory(String category) {
         categories.remove(category);
     }
 
     public void restoreDefault() {
 
-        System.out.println("Are you sure? This will remove any category added by you.\nType \"YES\" to delete, anything else will abort restoring.");
+        System.out.println("Are you sure? This will remove any category added by you.\n" +
+                "Type \"YES\" to delete, anything else will abort restoring.");
         Scanner scanner = new Scanner(System.in);
         if (scanner.next().equals("YES")) {
             categories.clear();
 
-            categories.add("Accommodation");         // <-- DEFAULT CATEGORIES (alphabetical order)
-            categories.add("Children");
-            categories.add("Clothes");
-            categories.add("Entertainment");
-            categories.add("Food");
-            categories.add("Healthcare");
-            categories.add("Hygiene");
-            categories.add("Loans");
-            categories.add("Medias");
-            categories.add("Others");
-            categories.add("Savings");
-            categories.add("Transportation");
+            readCSV readFromFile = new readCSV();
+            categories.addAll(readFromFile("DefaultCategories.txt"));
 
             System.out.println("Restored.");
 
         } else {
             System.out.println("Aborted.");
         }
+    }
+
+    private String readInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private boolean isAborted(String input) {
+        if(input.equals("q")) {
+            System.out.println("Aborted.");
+            return true;
+        }
+
+        return false;
     }
 }
