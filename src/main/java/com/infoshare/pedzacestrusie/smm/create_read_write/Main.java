20fa078737f928e4459a1d240a5de6f65f5fda40
Daniel Karaś <daniel.j.karas@gmail.com>
2018-02-09 16:57:43 +0100

    in work

--- src/main/java/com/infoshare/pedzacestrusie/smm/create_read_write/Main.java
+++ src/main/java/com/infoshare/pedzacestrusie/smm/create_read_write/Main.java
@@ -1,18 +1,27 @@
 package com.infoshare.pedzacestrusie.smm.create_read_write;
 
+import com.sun.xml.internal.ws.resources.SoapMessages;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        CreateCSV creator = new CreateCSV();
-        creator.createFile();
+//        CreateCSV creator = new CreateCSV();
+//        creator.createFile();
 
 //        ReadCSV reader = new ReadCSV();
-
+//        System.out.println(reader.readFromFile("marzec18.csv"));
 //
-//        WriteCSV writer = new WriteCSV();
-//        writer.writeToTheCsvFile("marzec18.csv");
+
+
+        WriteCSV writer = new WriteCSV();
+        writer.writeToTheCsvFile("marzec18.csv", sampleList);
 
     }
+
+
 }
 
--- src/main/java/com/infoshare/pedzacestrusie/smm/create_read_write/ReadCSV.java
+++ src/main/java/com/infoshare/pedzacestrusie/smm/create_read_write/ReadCSV.java
@@ -3,36 +3,32 @@ package com.infoshare.pedzacestrusie.smm.create_read_write;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class ReadCSV {
 
 
-    public String readFromFile(String spendingData) throws Exception {
+    public List<String> readFromFile(String spendingData) throws Exception {
+
+        List<String> readList = new ArrayList<String>();
 
         FileReader myFile = new FileReader(spendingData);
         BufferedReader fileReader = new BufferedReader(myFile);
 
-
         String line = "";
-        String temp = "";
 
         try {
             while ((line = fileReader.readLine()) != null) {
-                temp += line + "\n";
-
+                readList.add(line);
             }
-            System.out.println("Here is information read from file: ");
-            System.out.println();
-            System.out.println(temp);
-
+//
             fileReader.close();
 
         } catch (Exception e) {
             System.out.println("File is not found.");
         }
-        return temp;
-
+        return readList;
     }
 }
