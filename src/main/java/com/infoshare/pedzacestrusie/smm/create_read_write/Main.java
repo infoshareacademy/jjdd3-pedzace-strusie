8ca3cf8ae8a65a7d9743c8b064416e2a501cfe89
Daniel Karaś <daniel.j.karas@gmail.com>
2018-02-13 14:44:47 +0100

    Main maintain

--- src/main/java/com/infoshare/pedzacestrusie/smm/create_read_write/Main.java
+++ src/main/java/com/infoshare/pedzacestrusie/smm/create_read_write/Main.java
@@ -1,27 +1,9 @@
 package com.infoshare.pedzacestrusie.smm.create_read_write;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-//        CreateCSV creator = new CreateCSV();
-//        creator.createFile();
-
-        ReadCSV reader = new ReadCSV();
-        System.out.println(reader.readFromFileExpenses());
-
-
-
-//        List<Expense> expenseList = Arrays.asList(
-//                new Expense("2018-02-12", "food expense", 143.25),
-//                new Expense("2018-02-20", "gasoline", 189.45)
-//
-//        );
-//        WriteCSV writer = new WriteCSV();
-//        writer.writeToTheCsvFile("expenses18.csv", expenseList);
 
     }
 
