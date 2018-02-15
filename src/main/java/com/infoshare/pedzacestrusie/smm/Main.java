4382a29ff0031162cb8f6862d43fdf20ee83c366
Daniel Karaś <daniel.j.karas@gmail.com>
2018-02-15 21:41:31 +0100

    progress with write and load services for app properties

--- src/main/java/com/infoshare/pedzacestrusie/smm/Main.java
+++ src/main/java/com/infoshare/pedzacestrusie/smm/Main.java
@@ -4,7 +4,5 @@ public class Main {
 
     public static void main (String [] args){
 
-        PropertiesService service = new PropertiesService();
-        service.printProperties();
     }
 }
--- src/main/java/com/infoshare/pedzacestrusie/smm/PropertiesService.java
+++ src/main/java/com/infoshare/pedzacestrusie/smm/PropertiesService.java
@@ -1,49 +1,84 @@
 package com.infoshare.pedzacestrusie.smm;
 
-import java.io.IOException;
-import java.util.Properties;
-import java.util.SortedSet;
-import java.util.TreeSet;
-import java.util.Iterator;
+
+import java.io.*;
+import java.util.*;
 
 public class PropertiesService {
 
-        private final ClassLoader PROPERTIES_FILE = Thread.currentThread().getContextClassLoader();
-        private final String FILE = "pedzacestrusie.properties";
-        private Properties properties = new Properties();
-        private SortedSet<String> propertiesList = new TreeSet<>();
+    Properties properties = new Properties();
 
 
-//   public void PropertyService() {
-//
-//            try {
-//                loadProperties();
-//            }catch (IOException ioe){
-//                System.out.println("Where's the file?");
-//            }
-//        }
-//
-//        public SortedSet<String> getPropertiesList() {
-//            return propertiesList;
-//        }
-//
-//        private void loadProperties() throws IOException {
-//            properties.load(PROPERTIES_FILE.getResourceAsStream(FILE));
-//            Iterator<String> it = properties.stringPropertyNames().iterator();
-//            while(it.hasNext()) {
-//                propertiesList.add(properties.getProperty(it.next()));
-//            }
-//        }
-//
-//        public String getValue(String value) {
-//            return properties.getProperty(value);
-//        }
-//
-//        public void printProperties() {
-//            for(String key : properties.stringPropertyNames()) {
-//                String value = properties.getProperty(key);
-//                System.out.println(key + " => " + value);
-//            }
-//        }
+    public void writePropertyService(String nameOfCurrency, String dateFormat) {
+        OutputStream outputStream = null;
+
+        try {
+            outputStream = new FileOutputStream("config.properties");
+
+            try {
+
+                List<String> categoryList = addCategoryToList();
+
+                for (int i = 0; i < categoryList.size() - 1; i++) {
+                    properties.setProperty("name_of_category_" + (i + 1), categoryList.get(i));
+                }
+
+                properties.setProperty("currency", nameOfCurrency);
+                properties.setProperty("date-format", dateFormat);
+
+            } catch (NullPointerException e) {
+                System.out.println("There is problem with writing to file" + e.getMessage());
+            }
+            properties.store(outputStream, null);
+
+        } catch (IOException e) {
+            System.out.println("File does not exist in system!!!" + e.getMessage());
+        } finally {
+            if (outputStream != null)
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    System.out.println("There is some problem to update file with properties!!!" + e.getMessage());
+                }
+        }
     }
 
+    public void loadPropertyService() {
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream("config.properties");
+            properties.load(input);
+
+            List<String> propertiesList = new ArrayList<>();
+            Iterator<String> iterator = properties.stringPropertyNames().iterator();
+
+            while (iterator.hasNext()) {
+                propertiesList.add(properties.getProperty(iterator.next()));
+            }
+            System.out.println(propertiesList.toString());
+
+        } catch (IOException e) {
+            System.out.println("There is some problems with load the config data!!!" + e.getMessage());
+        }
+    }
+
+    private List<String> addCategoryToList() {
+
+        List<String> categoryList = new ArrayList<>();
+        Scanner source = new Scanner(System.in);
+        String categoryName = " ";
+
+
+        do {
+            System.out.println("If you want to stop enter category name, please type: exit ");
+            System.out.println("Please enter name of the category:  ");
+            categoryName = source.nextLine();
+            categoryList.add(categoryName);
+        }
+        while (!categoryName.equals("exit"));
+        return categoryList;
+    }
+
+
+}
