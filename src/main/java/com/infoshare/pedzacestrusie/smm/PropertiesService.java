4ea413792bf5c27122fc05ed6a6c160278fa9496
Daniel Karaś <daniel.j.karas@gmail.com>
2018-02-16 16:44:13 +0100

    property service - working version

--- src/main/java/com/infoshare/pedzacestrusie/smm/PropertiesService.java
+++ src/main/java/com/infoshare/pedzacestrusie/smm/PropertiesService.java
@@ -6,79 +6,30 @@ import java.util.*;
 
 public class PropertiesService {
 
-    Properties properties = new Properties();
+    private Properties properties = new Properties();
 
-
-    public void writePropertyService(String nameOfCurrency, String dateFormat) {
-        OutputStream outputStream = null;
-
-        try {
-            outputStream = new FileOutputStream("config.properties");
-
-            try {
-
-                List<String> categoryList = addCategoryToList();
-
-                for (int i = 0; i < categoryList.size() - 1; i++) {
-                    properties.setProperty("name_of_category_" + (i + 1), categoryList.get(i));
-                }
-
-                properties.setProperty("currency", nameOfCurrency);
-                properties.setProperty("date-format", dateFormat);
-
-            } catch (NullPointerException e) {
-                System.out.println("There is problem with writing to file" + e.getMessage());
-            }
-            properties.store(outputStream, null);
-
-        } catch (IOException e) {
-            System.out.println("File does not exist in system!!!" + e.getMessage());
-        } finally {
-            if (outputStream != null)
-                try {
-                    outputStream.close();
-                } catch (IOException e) {
-                    System.out.println("There is some problem to update file with properties!!!" + e.getMessage());
-                }
-        }
+    public PropertiesService() {
+        loadPropertyService();
     }
 
-    public void loadPropertyService() {
-        InputStream input = null;
+    public String getFormatDate() {
+        return this.properties.getProperty("date-format", "yyyy-MM-dd");
+    }
+
+    public String getCurrency() {
+        return this.properties.getProperty("currency", "PLN");
+    }
+
+    private Properties loadPropertyService() {
 
         try {
-            input = new FileInputStream("config.properties");
-            properties.load(input);
-
-            List<String> propertiesList = new ArrayList<>();
-            Iterator<String> iterator = properties.stringPropertyNames().iterator();
-
-            while (iterator.hasNext()) {
-                propertiesList.add(properties.getProperty(iterator.next()));
-            }
-            System.out.println(propertiesList.toString());
-
+            InputStream input = new FileInputStream("config.properties");
+            this.properties.load(input);
+            input.close();
         } catch (IOException e) {
             System.out.println("There is some problems with load the config data!!!" + e.getMessage());
         }
+        return null;
     }
-
-    private List<String> addCategoryToList() {
-
-        List<String> categoryList = new ArrayList<>();
-        Scanner source = new Scanner(System.in);
-        String categoryName = " ";
-
-
-        do {
-            System.out.println("If you want to stop enter category name, please type: exit ");
-            System.out.println("Please enter name of the category:  ");
-            categoryName = source.nextLine();
-            categoryList.add(categoryName);
-        }
-        while (!categoryName.equals("exit"));
-        return categoryList;
-    }
-
-
 }
+
