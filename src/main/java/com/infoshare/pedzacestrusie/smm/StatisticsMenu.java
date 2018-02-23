c6be45bac1b5f41c5ff6dc3ef77a2e0444e09eec
Maciej Wanagos <cod3bind@gmail.com>
2018-02-23 22:27:32 +0100

    JJDDPED-6 Added printAmountByDate, printAmountByDateByCategories and printAmountByCategories

--- src/main/java/com/infoshare/pedzacestrusie/smm/StatisticsMenu.java
+++ src/main/java/com/infoshare/pedzacestrusie/smm/StatisticsMenu.java
@@ -1,11 +1,9 @@
 package com.infoshare.pedzacestrusie.smm;
 
-import com.sun.org.apache.xpath.internal.SourceTree;
-
 import java.time.LocalDate;
 
 public class StatisticsMenu extends Menu {
-    private String[] statisticsMenuItems = {"Sum expenses by categories", "Sum expenses by date", "Sum expenses by months", "Exit"};
+    private String[] statisticsMenuItems = {"Expenses by categories", "Expenses by months", "Expenses and categories by months", "Exit"};
 
     @Override
     protected void executeMenu() {
@@ -18,24 +16,32 @@ public class StatisticsMenu extends Menu {
         switch (idx) {
             case 1: {
                 new Settings().updateExpenseListFromFile();
-                new StatisticsService().amountByCategory(UserRepository.getExpensesUserRepository());
+                new Settings().updateIncomeListFromFile();
+                System.out.println("Begin period:");
+                LocalDate minDatePeriod = new InputService().getLocalDate();
+                System.out.println("End period:");
+                LocalDate maxDatePeriod = new InputService().getLocalDate();
+                new StatisticsService().printAmountByCategoriesByPeriod(UserRepository.getExpensesUserRepository(), UserRepository.getIncomesUserRepository(), minDatePeriod, maxDatePeriod);
                 printMenu(setMenuDescription(statisticsMenuItems));
                 break;
             }
             case 2: {
                 new Settings().updateExpenseListFromFile();
-                //for test only
                 System.out.println("Begin period:");
                 LocalDate minDatePeriod = new InputService().getLocalDate();
                 System.out.println("End period:");
                 LocalDate maxDatePeriod = new InputService().getLocalDate();
-                new StatisticsService().amountByCategoryByPeriod(UserRepository.getExpensesUserRepository(),minDatePeriod,maxDatePeriod);
+                new StatisticsService().printAmountByDateByPeriod(UserRepository.getExpensesUserRepository(), minDatePeriod, maxDatePeriod);
                 printMenu(setMenuDescription(statisticsMenuItems));
                 break;
             }
             case 3: {
                 new Settings().updateExpenseListFromFile();
-                new StatisticsService().testAmountByCategory(UserRepository.getExpensesUserRepository());
+                System.out.println("Begin period:");
+                LocalDate minDatePeriod = new InputService().getLocalDate();
+                System.out.println("End period:");
+                LocalDate maxDatePeriod = new InputService().getLocalDate();
+                new StatisticsService().printAmountByDateByCategoriesByPeriod(UserRepository.getExpensesUserRepository(), minDatePeriod, maxDatePeriod);
                 printMenu(setMenuDescription(statisticsMenuItems));
                 break;
             }
--- src/main/java/com/infoshare/pedzacestrusie/smm/StatisticsService.java
+++ src/main/java/com/infoshare/pedzacestrusie/smm/StatisticsService.java
@@ -4,44 +4,44 @@ import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.toList;
-
 public class StatisticsService {
-    private List<Expense> userExpense = UserRepository.getExpensesUserRepository();
-    private List<Income> userIncome = UserRepository.getIncomesUserRepository();
-    private Set<String> userCategories = UserRepository.getCategoriesUserRepository();
+    public void printAmountByCategoriesByPeriod(List<Expense> expenses, List<Income> incomes, LocalDate minDatePeriod, LocalDate maxDatePeriod) {
+        List<Expense> periodExpensesList = expenses.stream()
+                .filter(i -> i.getDate().isBefore(maxDatePeriod) && i.getDate().isAfter(minDatePeriod))
+                .collect(Collectors.toList());
 
+        List<Income> periodIncomesList = incomes.stream()
+                .filter(i -> i.getDate().isBefore(maxDatePeriod) && i.getDate().isAfter(minDatePeriod))
+                .collect(Collectors.toList());
 
-    public void amountByCategory(List<Expense> expenses) {
+        printAmountByCategories(periodExpensesList, periodIncomesList);
+    }
+
+    private void printAmountByCategories(List<Expense> expenses, List<Income> incomes) {
         printPeriod(expenses);
 
         Map<String, Double> mapByCategories = expenses.stream()
                 .collect(Collectors.groupingBy(Expense::getCategories,
                         Collectors.summingDouble(Expense::getExpense)));
 
-        Map sortMap = mapByCategories.entrySet().stream()
+        mapByCategories.entrySet().stream()
                 .sorted(Map.Entry.comparingByKey())
-                .peek((i) -> System.out.printf("Category:%20s\t\tExpense:%12.2f%s%n", i.getKey(), i.getValue(), UserRepository.getCurrency()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+                .forEach((i) -> System.out.printf("Category:%20s,\t\tExpense:%12.2f%s%n", i.getKey(), i.getValue(), UserRepository.getCurrency()));
 
-        Double result = expenses.stream()
+        Double resultIncome = incomes.stream()
+                .mapToDouble(Income::getIncome)
+                .sum();
+
+        System.out.printf("%nTotal amount of revenue:%32.2f%s%n", resultIncome, UserRepository.getCurrency());
+        Double resultExpense = expenses.stream()
                 .mapToDouble(Expense::getExpense)
                 .sum();
 
-        System.out.printf("%nTotal amount of expenses:%31.2f%s%n", result, UserRepository.getCurrency());
+        System.out.printf("Total amount of expenditure:%28.2f%s%n", resultExpense, UserRepository.getCurrency());
+        System.out.printf("Total profit:%43.2f%s%n", resultIncome - resultExpense, UserRepository.getCurrency());
     }
 
-    public void amountByCategoryByPeriod(List<Expense> expenses, LocalDate minDatePeriod, LocalDate maxDatePeriod) {
-
-        List<Expense> periodList = expenses.stream()
-                .filter(i -> i.getDate().isBefore(maxDatePeriod) && i.getDate().isAfter(minDatePeriod))
-                .collect(toList());
-
-        amountByCategory(periodList);
-    }
-
-    public void printPeriod(List<Expense> periodList) {
+    private void printPeriod(List<Expense> periodList) {
         Optional<Expense> minDate = periodList.stream().min(Comparator.comparing(Expense::getDate));
         System.out.println("Min date: " + minDate.get().getDate());
 
@@ -51,23 +51,61 @@ public class StatisticsService {
         System.out.printf("Period date is from %s to %s.%n", String.valueOf(minDate.get().getDate()), String.valueOf(maxDate.get().getDate()));
     }
 
-    public void testAmountByCategory(List<Expense> expenses) {
+    public void printAmountByDateByPeriod(List<Expense> expenses, LocalDate minDatePeriod, LocalDate maxDatePeriod) {
+        List<Expense> periodList = expenses.stream()
+                .filter(i -> i.getDate().isBefore(maxDatePeriod) && i.getDate().isAfter(minDatePeriod))
+                .collect(Collectors.toList());
+
+        printAmountByDate(periodList);
+    }
+
+    private void printAmountByDate(List<Expense> expenses) {
         printPeriod(expenses);
 
         Map<String, Double> mapByCategories = expenses.stream()
-                .collect(Collectors.groupingBy(e -> e.getDate().getMonth() + ", " + e.getDate().getYear(),
+                .collect(Collectors.groupingBy(e -> String.format("%d, %tm(%s)", e.getDate().getYear(), e.getDate().getMonth(), e.getDate().getMonth()),
                         Collectors.summingDouble(Expense::getExpense)));
 
-        Map sortMap = mapByCategories.entrySet().stream()
+        mapByCategories.entrySet().stream()
                 .sorted(Map.Entry.comparingByKey())
-                .peek((i) -> System.out.printf("M:%20s\t\tExpense:%12.2f%s%n", i.getKey(), i.getValue(), UserRepository.getCurrency()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+                .forEach((i) -> System.out.printf("Month:%24s.\t\tExpense:%12.2f%s%n", i.getKey(), i.getValue(), UserRepository.getCurrency()));
 
         Double result = expenses.stream()
                 .mapToDouble(Expense::getExpense)
                 .sum();
 
-        System.out.printf("%nTotal amount of expenses:%31.2f%s%n", result, UserRepository.getCurrency());
+        System.out.printf("%nTotal amount of expenditure:%28.2f%s%n", result, UserRepository.getCurrency());
+    }
+
+    public void printAmountByDateByCategoriesByPeriod(List<Expense> expenses, LocalDate minDatePeriod, LocalDate maxDatePeriod) {
+        List<Expense> periodList = expenses.stream()
+                .filter(i -> i.getDate().isBefore(maxDatePeriod) && i.getDate().isAfter(minDatePeriod))
+                .collect(Collectors.toList());
+
+        printAmountByDateByCategories(periodList);
+    }
+
+    private void printAmountByDateByCategories(List<Expense> expenses) {
+        printPeriod(expenses);
+
+        Map<String, Map<String, Double>> mapByCategories = expenses.stream()
+                .collect(Collectors.groupingBy(e -> String.format("%d, %tm(%s)", e.getDate().getYear(), e.getDate().getMonth(), e.getDate().getMonth()),
+                        Collectors.groupingBy(Expense::getCategories, Collectors.summingDouble(Expense::getExpense))));
+
+        Map<String, Map<String, Double>> sortMap = mapByCategories.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+        sortMap.forEach((key, value) -> {
+            System.out.printf("%nDate:%20s%n", key);
+            value.forEach((keyInside, valueInside) -> System.out.printf("Category:%20s,\t\tExpense:%12.2f%s%n", keyInside, valueInside, UserRepository.getCurrency()));
+        });
+
+        Double result = expenses.stream()
+                .mapToDouble(Expense::getExpense)
+                .sum();
+
+        System.out.printf("%nTotal amount of expenditure:%28.2f%s%n", result, UserRepository.getCurrency());
     }
 }
