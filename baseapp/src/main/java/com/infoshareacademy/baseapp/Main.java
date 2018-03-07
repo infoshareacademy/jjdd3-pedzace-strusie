4a8b2c80bcd30a571534ecda944a5243fbd7003c
Maciej Wanagos <cod3bind@gmail.com>
2018-03-07 02:37:48 +0100

    Added new structure for web app

--- baseapp/src/main/java/com/infoshareacademy/baseapp/Main.java
+++ baseapp/src/main/java/com/infoshareacademy/baseapp/Main.java
@@ -6,7 +6,9 @@ import org.slf4j.LoggerFactory;
 public class Main {
 
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger("Example App");
+
+        Logger logger = LoggerFactory.getLogger(Main.class);
+
         logger.info("'sup? I'm your info logger");
         logger.debug("hey HEY hey! I'm your debug logger");
         new Settings().updateDefaults(args);
