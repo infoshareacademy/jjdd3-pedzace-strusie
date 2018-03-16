c3d3b63985d08fecb8c4e4fe87dc2b92437e5608
Maciej Wanagos <cod3bind@gmail.com>
2018-03-17 00:47:18 +0100

    JJDDPED-23 Update StatisticsService

--- baseapp/src/main/java/com/infoshareacademy/baseapp/StatisticsMenu.java
+++ baseapp/src/main/java/com/infoshareacademy/baseapp/StatisticsMenu.java
@@ -17,79 +17,62 @@ public class StatisticsMenu extends Menu {
             case 1: {
                 new Settings().updateExpenseListFromFile();
                 new Settings().updateIncomeListFromFile();
-                LocalDate minDatePeriod;
-                LocalDate maxDatePeriod;
 
-                while (true) {
-                    System.out.println("Begin period:");
-                    minDatePeriod = new InputService().getLocalDate();
-                    System.out.println("End period:");
-                    maxDatePeriod = new InputService().getLocalDate();
+                System.out.println("Begin period:");
+                LocalDate minDatePeriod = new InputService().getLocalDate();
+                System.out.println("End period:");
+                LocalDate maxDatePeriod = new InputService().getLocalDate();
 
-                    LocalDate tempDate = maxDatePeriod;
-                    if (maxDatePeriod.isBefore(minDatePeriod)) {
-                        maxDatePeriod = minDatePeriod;
-                        minDatePeriod = tempDate;
-                    }
-                    if (!minDatePeriod.isEqual(maxDatePeriod)) {
-                        break;
-                    } else {
-                        System.out.println("Date should be different!");
-                    }
+                LocalDate tempDate = maxDatePeriod;
+                if (maxDatePeriod.isBefore(minDatePeriod)) {
+                    maxDatePeriod = minDatePeriod;
+                    minDatePeriod = tempDate;
                 }
-                new StatisticsService().printAmountByCategoriesByPeriod(UserRepository.getExpensesUserRepository(), UserRepository.getIncomesUserRepository(), minDatePeriod, maxDatePeriod);
+
+                new StatisticsService().printAmountByCategoriesByPeriod(UserRepository.getExpensesUserRepository(),
+                        UserRepository.getIncomesUserRepository(),
+                        minDatePeriod,
+                        maxDatePeriod);
                 printMenu(setMenuDescription(statisticsMenuItems));
                 break;
             }
             case 2: {
                 new Settings().updateExpenseListFromFile();
-                LocalDate minDatePeriod;
-                LocalDate maxDatePeriod;
 
-                while (true) {
-                    System.out.println("Begin period:");
-                    minDatePeriod = new InputService().getLocalDate();
-                    System.out.println("End period:");
-                    maxDatePeriod = new InputService().getLocalDate();
+                System.out.println("Begin period:");
+                LocalDate minDatePeriod = new InputService().getLocalDate();
+                System.out.println("End period:");
+                LocalDate maxDatePeriod = new InputService().getLocalDate();
 
-                    LocalDate tempDate = maxDatePeriod;
-                    if (maxDatePeriod.isBefore(minDatePeriod)) {
-                        maxDatePeriod = minDatePeriod;
-                        minDatePeriod = tempDate;
-                    }
-                    if (!minDatePeriod.isEqual(maxDatePeriod)) {
-                        break;
-                    }else {
-                        System.out.println("Date should be different!");
-                    }
+                LocalDate tempDate = maxDatePeriod;
+                if (maxDatePeriod.isBefore(minDatePeriod)) {
+                    maxDatePeriod = minDatePeriod;
+                    minDatePeriod = tempDate;
                 }
-                new StatisticsService().printAmountByDateByPeriod(UserRepository.getExpensesUserRepository(), minDatePeriod, maxDatePeriod);
+                new StatisticsService().printAmountByDateByPeriod(UserRepository.getExpensesUserRepository(),
+                        UserRepository.getIncomesUserRepository(),
+                        minDatePeriod,
+                        maxDatePeriod);
                 printMenu(setMenuDescription(statisticsMenuItems));
                 break;
             }
             case 3: {
                 new Settings().updateExpenseListFromFile();
-                LocalDate minDatePeriod;
-                LocalDate maxDatePeriod;
 
-                while (true) {
-                    System.out.println("Begin period:");
-                    minDatePeriod = new InputService().getLocalDate();
-                    System.out.println("End period:");
-                    maxDatePeriod = new InputService().getLocalDate();
+                System.out.println("Begin period:");
+                LocalDate minDatePeriod = new InputService().getLocalDate();
+                System.out.println("End period:");
+                LocalDate maxDatePeriod = new InputService().getLocalDate();
 
-                    LocalDate tempDate = maxDatePeriod;
-                    if (maxDatePeriod.isBefore(minDatePeriod)) {
-                        maxDatePeriod = minDatePeriod;
-                        minDatePeriod = tempDate;
-                    }
-                    if (!minDatePeriod.isEqual(maxDatePeriod)) {
-                        break;
-                    }else {
-                        System.out.println("Date should be different!");
-                    }
+                LocalDate tempDate = maxDatePeriod;
+                if (maxDatePeriod.isBefore(minDatePeriod)) {
+                    maxDatePeriod = minDatePeriod;
+                    minDatePeriod = tempDate;
                 }
-                new StatisticsService().printAmountByDateByCategoriesByPeriod(UserRepository.getExpensesUserRepository(), minDatePeriod, maxDatePeriod);
+                new StatisticsService().printAmountByDateByCategoriesByPeriod(UserRepository.getExpensesUserRepository(),
+                        UserRepository.getIncomesUserRepository(),
+                        minDatePeriod,
+                        maxDatePeriod);
                 printMenu(setMenuDescription(statisticsMenuItems));
                 break;
             }
--- baseapp/src/main/java/com/infoshareacademy/baseapp/StatisticsService.java
+++ baseapp/src/main/java/com/infoshareacademy/baseapp/StatisticsService.java
@@ -43,6 +43,10 @@ public class StatisticsService {
                 .sorted(Map.Entry.comparingByKey())
                 .forEach((i) -> System.out.printf("Category:%20s,\t\tExpense:%12.2f%s%n", i.getKey(), i.getValue(), UserRepository.getCurrency()));
 
+        printResults(expenses, incomes);
+    }
+
+    private void printResults(List<Expense> expenses, List<Income> incomes) {
         Double resultIncome = getSumOfIncomes(incomes);
         System.out.printf("%nTotal amount of revenue:%32.2f%s%n", resultIncome, UserRepository.getCurrency());
 
@@ -53,33 +57,36 @@ public class StatisticsService {
 
     public Double getSumOfExpenses(List<Expense> expenses) {
         return expenses.stream()
-                    .mapToDouble(Expense::getExpense)
-                    .sum();
+                .mapToDouble(Expense::getExpense)
+                .sum();
     }
 
     public Double getSumOfIncomes(List<Income> incomes) {
         return incomes.stream()
-                    .mapToDouble(Income::getIncome)
-                    .sum();
+                .mapToDouble(Income::getIncome)
+                .sum();
     }
 
     private void printPeriod(List<Expense> periodList) {
-        Optional<Expense> minDate = periodList.stream().min(Comparator.comparing(Expense::getDate));
-        System.out.println("Min date: " + minDate.get().getDate());
+        if (periodList.size() == 0) {
+            System.out.println("No expenses for this period!");
+        } else {
+            Optional<Expense> minDate = periodList.stream().min(Comparator.comparing(Expense::getDate));
+            Optional<Expense> maxDate = periodList.stream().max(Comparator.comparing(Expense::getDate));
 
-        Optional<Expense> maxDate = periodList.stream().max(Comparator.comparing(Expense::getDate));
-        System.out.println("Max date: " + maxDate.get().getDate());
-
-        System.out.printf("Period date is from %s to %s.%n", String.valueOf(minDate.get().getDate()), String.valueOf(maxDate.get().getDate()));
+            System.out.printf("Period date is from %s to %s.%n", String.valueOf(minDate.get().getDate()), String.valueOf(maxDate.get().getDate()));
+        }
     }
 
-    public void printAmountByDateByPeriod(List<Expense> expenses, LocalDate minDatePeriod, LocalDate maxDatePeriod) {
-        List<Expense> periodList = getPeriodExpenses(expenses, minDatePeriod, maxDatePeriod);
+    public void printAmountByDateByPeriod(List<Expense> expenses, List<Income> incomes, LocalDate minDatePeriod, LocalDate maxDatePeriod) {
+        List<Expense> periodExpensesList = getPeriodExpenses(expenses, minDatePeriod, maxDatePeriod);
 
-        printAmountByDate(periodList);
+        List<Income> periodIncomesList = getPeriodIncomes(incomes, minDatePeriod, maxDatePeriod);
+
+        printAmountByDate(periodExpensesList, periodIncomesList);
     }
 
-    private void printAmountByDate(List<Expense> expenses) {
+    private void printAmountByDate(List<Expense> expenses, List<Income> incomes) {
         printPeriod(expenses);
 
         Map<String, Double> mapByCategories = expenses.stream()
@@ -90,18 +97,18 @@ public class StatisticsService {
                 .sorted(Map.Entry.comparingByKey())
                 .forEach((i) -> System.out.printf("Month:%24s.\t\tExpense:%12.2f%s%n", i.getKey(), i.getValue(), UserRepository.getCurrency()));
 
-        Double result = getSumOfExpenses(expenses);
-
-        System.out.printf("%nTotal amount of expenditure:%28.2f%s%n", result, UserRepository.getCurrency());
+        printResults(expenses, incomes);
     }
 
-    public void printAmountByDateByCategoriesByPeriod(List<Expense> expenses, LocalDate minDatePeriod, LocalDate maxDatePeriod) {
-        List<Expense> periodList = getPeriodExpenses(expenses, minDatePeriod, maxDatePeriod);
+    public void printAmountByDateByCategoriesByPeriod(List<Expense> expenses, List<Income> incomes, LocalDate minDatePeriod, LocalDate maxDatePeriod) {
+        List<Expense> periodExpensesList = getPeriodExpenses(expenses, minDatePeriod, maxDatePeriod);
 
-        printAmountByDateByCategories(periodList);
+        List<Income> periodIncomesList = getPeriodIncomes(incomes, minDatePeriod, maxDatePeriod);
+
+        printAmountByDateByCategories(periodExpensesList, periodIncomesList);
     }
 
-    private void printAmountByDateByCategories(List<Expense> expenses) {
+    private void printAmountByDateByCategories(List<Expense> expenses, List<Income> incomes) {
         printPeriod(expenses);
 
         Map<String, Map<String, Double>> mapByCategories = expenses.stream()
@@ -118,8 +125,6 @@ public class StatisticsService {
             value.forEach((keyInside, valueInside) -> System.out.printf("Category:%20s,\t\tExpense:%12.2f%s%n", keyInside, valueInside, UserRepository.getCurrency()));
         });
 
-        Double result = getSumOfExpenses(expenses);
-
-        System.out.printf("%nTotal amount of expenditure:%28.2f%s%n", result, UserRepository.getCurrency());
+        printResults(expenses, incomes);
     }
 }
