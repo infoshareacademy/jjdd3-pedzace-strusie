b6fa9d27bfaa42295cc429bb9ca39148f71bf064
danieljkaras <35041653+danieljkaras@users.noreply.github.com>
2018-03-16 09:21:09 +0100

    Validation data filter (#40)
    * validation fixes
    
    * validation data complete, adjusting properties in addincome and addexpenese servlet
    
    * fixes for validation filters

--- webapp/src/main/java/com/infoshareacademy/webapp/servlets/AddExpenseServlet.java
+++ webapp/src/main/java/com/infoshareacademy/webapp/servlets/AddExpenseServlet.java
@@ -1,18 +1,43 @@
 package com.infoshareacademy.webapp.servlets;
 
+import com.infoshareacademy.baseapp.Expense;
+import com.infoshareacademy.webapp.dao_lockal.CategoryDaoLoc;
+import com.infoshareacademy.webapp.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+
 
 @WebServlet("/add-expense")
 
 public class AddExpenseServlet extends HttpServlet {
 
-  /*  private static final Logger logger = Logger.getLogger(getClass().getName());
+    private static final Logger logger = LoggerFactory.getLogger(AddExpenseServlet.class);
 
-    Template template;
+    private Template template;
 
     @EJB
     private ExpenseDaoBean expenseDaoBean;
+
+    @EJB
+    CategoryDaoLoc categoryDaoLoc;
 
 
     @Override
@@ -21,7 +46,7 @@ public class AddExpenseServlet extends HttpServlet {
         try {
             template = TemplateProvider.createTemplate(getServletContext(), "expense-service.ftlh");
         } catch (IOException e) {
-            logger.log(Level.SEVERE, e.getMessage(), e);
+            logger.error(e.getMessage());
         }
     }
 
@@ -33,15 +58,13 @@ public class AddExpenseServlet extends HttpServlet {
         List<String> errors = (List<String>) req.getSession().getAttribute("errors");
         if (errors != null && !errors.isEmpty()) {
             dataModel.put("errors", errors);
-            dataModel.put("user", req.getSession().getAttribute("user"));
-            req.getSession().removeAttribute("error");
-            req.getSession().removeAttribute("user");
+            req.getSession().removeAttribute("errors");
         }
 
         try {
             template.process(dataModel, printWriter);
         } catch (TemplateException e) {
-            logger.log(Level.SEVERE, e.getMessage(), e);
+            logger.error(e.getMessage(), e);
         }
     }
 
@@ -53,9 +76,9 @@ public class AddExpenseServlet extends HttpServlet {
         expense.setCategory(req.getParameter("category"));
         expense.setExpense(Double.parseDouble(req.getParameter("expense")));
 
-        expenseDaoBean.addExpense(expense);
+//        expenseDaoBean.addExpense(expense);
 
-        resp.sendRedirect("/budget/add-expense");// tutaj należy wstawić odnośnik do servletu odpowiedzialnego za
+        resp.sendRedirect("/");// tutaj należy wstawić odnośnik do servletu odpowiedzialnego za
         // wyświetlanie wydatków użytkownika
-    }*/
+    }
 }
