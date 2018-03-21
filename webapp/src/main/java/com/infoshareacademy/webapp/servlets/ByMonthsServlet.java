14c6f2c143f53a6f873081191e83e1f1eff5f596
Daniel Karaś <daniel.j.karas@gmail.com>
2018-03-21 16:49:23 +0100

    add charts to the app view

--- webapp/src/main/java/com/infoshareacademy/webapp/servlets/ByMonthsServlet.java
+++ webapp/src/main/java/com/infoshareacademy/webapp/servlets/ByMonthsServlet.java
@@ -1,11 +1,14 @@
 package com.infoshareacademy.webapp.servlets;
 
+import com.infoshareacademy.webapp.dao.ExpenseDao;
 import com.infoshareacademy.webapp.dao_lockal.StatisticsDaoLoc;
 import com.infoshareacademy.webapp.freemarker.TemplateProvider;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+import model.Expense;
 
 import javax.ejb.EJB;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @WebServlet("/by-months")
@@ -20,6 +24,7 @@ public class ByMonthsServlet extends HttpServlet {
 
     @EJB
     StatisticsDaoLoc statisticsDaoLoc;
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -31,6 +36,7 @@ public class ByMonthsServlet extends HttpServlet {
         dataModel.put("maps", stringDoubleMap.entrySet());
         dataModel.put("sumExpenses", sumExpenses);
         dataModel.put("sumIncomes", sumIncomes);
+        dataModel.put("chartData", getChartData());  // odpowiada za rysowanie wykresu;
 
         Template template = TemplateProvider.createTemplate(getServletContext(), "by-months.ftlh");
 
@@ -40,4 +46,18 @@ public class ByMonthsServlet extends HttpServlet {
             e.printStackTrace();
         }
     }
+
+
+    private Map<String, Double> getChartData() {
+
+        Map data = new HashMap<String, Double>();
+        data.put("dla dzieci", 320.2d);
+        data.put("jedzenie", 12.2d);
+        data.put("paliwo", 350.50d);
+        data.put("mieszkanie", 520d);
+
+        return data;
+    }
+
+
 }
